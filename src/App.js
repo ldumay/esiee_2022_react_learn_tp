d476930c7e742d5fa7c37fc5f4435ba771e722e9
Maxence <maxence.hennekein@orange.fr>
2022-01-25 23:51:08 +0100

    Premier routage
    Il faut utiliser nos composants contenant des Link dans le Router
    Cela permet de faire le lien avec Routes, qui contient le chemin à emprunter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from "./components/Home";
+import About from "./components/About";
+import Header from "./components/Header";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/about" element={<About />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App;
