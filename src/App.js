8b83c6a65ada83e1b1aad15c32f8689d021c9f20
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-01-25 17:32:11 +0100

    Accueille

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Participer au concours 
         </a>
       </header>
     </div>
