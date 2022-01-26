691a41c09b9d149d6eb1bd514038427780b9b975
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-01-26 23:29:54 +0100

    Update RegisterCheckUser.js
    add function checkmail

--- src/components/views/RegisterCheckUser.js
+++ src/components/views/RegisterCheckUser.js
@@ -1,13 +1,35 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import 'react-phone-number-input/style.css'
+import Header from './Header'
 //const [inputEmail, setEmail] = useState();
 
 function RegisterCheckUser(props){
     const userInJson = "TOTO@TOTO.fr"
 
+    function Checkmail(){
+        if (props.inputEmail == userInJson){
+            console.log("Utilisateur existant");
+            //here call component register and change the URL
+        }
+        else {
+            console.log("Email non existant, bienvenue sur notre site!!!!!!!!");
+            //call component quiz and change the URL
+        }
+    }
     return (
         <div>
-            <h1>CheckUser Sheet</h1>
-            Mail : {props.inputEmail}
+            <Header />
+            <Container>
+                <div className="page">
+                    <Row>
+                        <Col>
+                            <h1>CheckUser Sheet</h1>
+                            <p>Mail : {props.inputEmail}</p>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+            {Checkmail()}
         </div> 
     )
 }
