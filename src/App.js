68a1f1fa0c1ad8347fae63430eb9a6b63f6859f1
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-01-26 10:07:52 +0100

    suppression
    suppression de main et invoices

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import Home from './components/views/Home';
-import Header from "./components/views/Header";
-import Register from './components/views/Register';
-import Result from './components/views/Result';
-import Films from './components/views/Films';
+import Home       from './components/views/Home';
+import Header     from "./components/views/Header";
+import Register   from './components/views/Register';
+import Result     from './components/views/Result';
+import Films      from './components/views/Films';
 import FilmsInfos from './components/views/FilmsInfos';
-import Wishlist from './components/views/Wishlist';
+import Wishlist   from './components/views/Wishlist';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Header />
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/result" element={<Result />} />
-				<Route path="/films" element={<Films />} />
+				<Route path="/"           element={<Home />} />
+				<Route path="/register"   element={<Register />} />
+				<Route path="/result"     element={<Result />} />
+				<Route path="/films"      element={<Films />} />
 				<Route path="/film-infos" element={<FilmsInfos />} />
-				<Route path="/wishlist" element={<Wishlist />} />
+				<Route path="/wishlist"   element={<Wishlist />} />
 			</Routes>
 		</BrowserRouter>
 	)
