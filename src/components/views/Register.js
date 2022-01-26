f21e77e17e146097ea7d7bd4d9b201f6840a6f2c
CHEN <91790376+CHENZhihao98@users.noreply.github.com>
2022-01-26 14:26:50 +0100

    installe package Phoneinput

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -40,6 +40,7 @@ function Register(){
                 const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 if (value.match(regEmail)){
                     console.log("Bon format")
+                    
                 }
                 else{
                     console.log("mauvais format")
