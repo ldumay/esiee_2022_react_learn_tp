a314b02d6dfff04dac3ec72abbc3ea37479d0979
Loic DUMAY <dumay.famille@gmail.com>
2022-01-26 16:31:07 +0100

    MAJ du thème

--- src/components/views/Wishlist.js
+++ src/components/views/Wishlist.js
@@ -1,4 +1,5 @@
-import { Container, Row } from "react-bootstrap"
+import '../../styles/Catalog.scss'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import Film from "../models/Film"
 import FilmsInfos from "../views/FilmsInfos"
 import Header from "./Header"
@@ -24,16 +25,7 @@ function Wishlist(currentUser) {
 			dateSortie: new Date(2022, 1, 26).toDateString(),
 			description: "description",
 			ratio: 25
-		},
-		{
-			id: 3,
-			nom: "Film",
-			imageUrl: "img.png",
-			genre: "action",
-			dateSortie: new Date(2022, 1, 26).toDateString(),
-			description: "description",
-			ratio: 35
-		},
+		}
 	]
 
 	return (
@@ -43,11 +35,32 @@ function Wishlist(currentUser) {
                 <div className="page">
                     <Row>
                         {filmList.map((film) => (
-                            <li key={`${film.id}-${film.nom}`} style={{ border: "1px, black" }}>
-                                <FilmsInfos film={film} />
-                            </li>
+                            <div className="catalog" key={`${film.id}-${film.nom}`} style={{ border: "1px, black" }}>
+                                
+                                {/* <FilmsInfos film={film} /> */}
+
+                                {/* Modèle d'une affiche de film */}
+                                <Col md="3" className="choixFilm">
+                                    <a href="#"
+                                        >
+                                        <div className="choixFilmItem">
+                                            <img
+                                                className="imageUrl"
+                                                alt="imageUrl"
+                                                src="https://images-na.ssl-images-amazon.com/images/I/91Rk2MjVnyL._RI_.jpg"
+                                            />
+                                            <h3>Film #1</h3>
+                                        </div>
+                                    </a>
+                                    <Button variant="danger" className="btnSupprFilm">
+                                        Retirer
+                                    </Button>
+                                </Col>
+
+                            </div>
                         )
                         )}
+
                     </Row>
                 </div>
             </Container>
